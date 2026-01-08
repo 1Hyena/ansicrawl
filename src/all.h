ab804d62e98a9da99266d58b31928a7a36ac6f0e
1Hyena <hyena@hyena.net.ee>
2026-01-08 21:33:24 +0200

    renamed client and server source files to better reflect their meaning

--- src/all.h
+++ src/all.h
@@ -2,14 +2,14 @@
 #ifndef ALL_H_06_01_2026
 #define ALL_H_06_01_2026
 ////////////////////////////////////////////////////////////////////////////////
+#include "client.h"
 #include "clip.h"
 #include "flags.h"
 #include "global.h"
 #include "log.h"
 #include "mem.h"
-#include "obj-client.h"
-#include "obj-server.h"
 #include "obj-user.h"
+#include "server.h"
 #include "signals.h"
 #include "string.h"
 #include "terminal.h"
